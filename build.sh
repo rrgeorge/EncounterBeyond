6bde4e7a2f59eeb8550aabec3f0a67cbe70d607e
Robert George <rgeorge@midnightweb.net>
2020-08-05 16:23:44 -0700

    add background.js to firefox build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 version=`jq -r .version manifest_ff.json`
 cp manifest_ff.json manifest.json
-zip encounterbeyond.ff.${version}.zip encounter*.png hostgrab.js manifest.json options.* rollgrabber.js
+zip encounterbeyond.ff.${version}.zip encounter*.png hostgrab.js manifest.json options.* rollgrabber.js background.js
 rm manifest.json
 cp manifest_chrome.json manifest.json
 zip encounterbeyond.chrome.${version}.zip encounter*.png hostgrab.js manifest.json options.* rollgrabber.js background.js
